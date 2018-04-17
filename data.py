12e77d7ce4f4100ad64a5c14eb209ef446ce3199
Zach Purcell <zmpurcell97@gmail.com>
2018-04-17 09:23:59 -0400

    Training works. Plots.

--- data.py
+++ data.py
@@ -1,103 +1,77 @@
 #!/usr/local/bin/python3
+
 import csv
 import numpy as np
 import pickle
 from collections import Counter
 import math
 
-'''
-Timestamp, Open, High, Low,	Close, Volume_(BTC), Volume_(Currency)
-'''
 def sample_handling():
 
     featureset = []
 
-    with open("data.csv", 'rt') as csvfile:
+    with open('data.csv', 'rt') as csvfile:
         reader = csv.reader(csvfile, delimiter=',')
 
-        current_high = 0.0
-        current_low = 0.0
-        current_open = 0.0
-        current_close = 0.0
-        current_volume_btc = 0.0
-        current_volume_currency = 0.0
-        current_net_change = 0.0 #will become difference between current_open and current_close
+        current_high = 0.0 #holds the current high of the row/day
+        current_low = 0.0 #holds the current low of the row/day
+        current_open = 0.0 #holds the current open of the row/day
+        current_close = 0.0 #holds the current close of the row/day
+        current_net_change = current_close - current_open #difference between current_open and current_close (this is the AVERAGE change through the day)
 
-        prev_high = 0.0
-        prev_low = 0.0
-        prev_open = 0.0
-        prev_close = 0.0
-        prev_volume_btc = 0.0
-        prev_volume_currency = 0.0
-        prev_delta_high = 0.0
+        prev_high = 0.0 #holds the previous row's/day's high value
+        prev_low = 0.0 #holds the previous row's/day's low value
+        prev_open = 0.0 #holds the previous row's/day's open value
+        prev_close = 0.0 #holds the previous row's/day's close value
+        prev_delta_high = 0.0 #holds the differences of the previous high and the high before that (in the nth row/day, it holds the difference of (n-1) and (n-2))
+        prev_net_change = 0.0
 
-        delta_high = 0.0
-        delta_low = 0.0
-        delta_open = 0.0
-        delta_close = 0.0
-        delta_volume_btc = 0.0
-        delta_volume_currency = 0.0
+        delta_high = 0.0 #holds change in high value between current row and previous row
+        delta_low = 0.0 #holds change in low value between current row and previous row
+        delta_open = 0.0 #holds change in open value between current row and previous row
+        delta_close = 0.0 #holds change in close value between current row and previous row
 
+        #counter variable
         counter = 0
 
-        indicator = 0 # -1 for bad, 0 for ok, 1 for good
+        indicator = 0 #
 
-        for row in reader:
-            #set current variables to current values from row
+        rows = list(reader)[-100000:] #rows is the last 100000 elements of reader, and thus the last 100000 rows of the csv file (most up to date data we have)
+        #rows = list(reader)[-150000:] #last 150000 elements of reader
+        for row in rows:
+
             current_high = float(row[2])
             current_low = float(row[3])
             current_open = float(row[1])
             current_close = float(row[4])
-            current_volume_btc = float(row[5])
-            current_volume_currency = float(row[6])
-            if counter > 0:
-                #calculated delta values
-                delta_high = (current_high - prev_high)/prev_high
-                delta_low = (current_low - prev_low)/prev_low
-                delta_open = (current_open - prev_open)/prev_open
-                detla_close = (current_close - prev_close)/prev_open
-                delta_volume_btc = (current_volume_btc - prev_volume_btc)/prev_volume_btc
-                delta_volume_currency = (current_volume_currency - prev_volume_currency)/prev_volume_currency
-                #calculate current change values
-                current_net_change = (current_close - current_open)/current_open
-                #append the vector the the list featureset
-                #featureset.append([delta_high, delta_low, delta_open, delta_close, delta_volume_btc, delta_volume_currency, current_net_change])
-                #assign indicator here
-                if prev_delta_high <= 0:
-                    indicator = -1
-                elif prev_delta_high > 0 and prev_delta_high < 1:
-                    indicator = 0
-                else:
-                    indicator = 1
-                featureset.append([[prev_delta_high, prev_high], [indicator, current_high]])
 
-            #set previous values to current so that they are ready for the next loop through
+            if counter > 0:
+                delta_high = current_high - prev_high
+                delta_low = current_low - prev_low
+                delta_open = current_open - prev_open
+                delta_close = current_close - prev_close
+                current_net_change = current_close - current_open
+
+                if current_net_change > 0:
+                    indicator = 1
+                else:
+                    indicator = -1
+
+                featureset.append([[prev_delta_high, prev_high, indicator], [current_high]])
+
             prev_high = current_high
             prev_low = current_low
             prev_open = current_open
             prev_close = current_close
-            prev_volume_btc = current_volume_btc
-            prev_volume_currency = current_volume_currency
             prev_delta_high = delta_high
+            prev_net_change = current_net_change
 
-            #increment counter
             counter += 1
 
         return featureset
 
-
 def create_feature_sets_and_labels(test_size=0.1):
-    features = sample_handling() #2d array
-
-    # features = np.array(features)
-    #
-    # testing_size = int(test_size * len(features))
-    #
-    # train_x = list(features[:,0][:-testing_size])
-    # train_y = list(features[:,1][:-testing_size])
-    #
-    # test_x = list(features[:,0][-testing_size:])
-    # test_y = list(features[:,1][-testing_size:])
+    features = sample_handling()
 
     train_x = []
     train_y = []
@@ -109,9 +83,7 @@ def create_feature_sets_and_labels(test_size=0.1):
         train_y.append(features[value][1])
 
     for value in range(0, int(math.ceil(test_size*len(features)))):
-        test_x.append(features[value][0])
-        test_y.append(features[value][1])
-
-    print(test_x)
+        test_x.append(features[-value][0])
+        test_y.append(features[-value][1])
 
     return train_x, train_y, test_x, test_y
--- train.py
+++ train.py
@@ -2,25 +2,21 @@
 import tensorflow as tf
 import numpy as np
 from data import create_feature_sets_and_labels
+import numpy as np
+import matplotlib.pyplot as plt
 
 train_x, train_y, test_x, test_y = create_feature_sets_and_labels()
 
-n_nodes_hl1 = 500
-n_nodes_hl2 = 500
-n_nodes_hl3 = 500
+n_nodes_hl1 = 6
+n_nodes_hl2 = 6
+n_nodes_hl3 = 6
 
-n_classes = 2
+n_classes = 1
 batch_size = 100
 
 x = tf.placeholder('float', [None, len(train_x[0])])
-y = tf.placeholder('float', [None, len(train_y[0])])
-
-'''
-print('length train_x: ', len(train_x)) # = 1416846
-print('length train_y: ', len(train_y)) # = 1416846
-print('length train_x[0]: ', len(train_x[0])) # = 2
-print('length train_y[0]: ', len(train_y[0])) # = 2
-'''
+y = tf.placeholder('float', [None, 1])
+z = tf.placeholder('float', [None, 1])
 
 def neural_network_model(data):
 
@@ -28,19 +24,17 @@ def neural_network_model(data):
                       'biases': tf.Variable(tf.random_normal([n_nodes_hl1]))}
 
     hidden_2_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl1, n_nodes_hl2])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl2]))}
+                      'biases' :tf.Variable(tf.random_normal([n_nodes_hl2]))}
 
     hidden_3_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl2, n_nodes_hl3])),
-                      'biases': tf.Variable(tf.random_normal([n_nodes_hl3]))}
+                      'biases' :tf.Variable(tf.random_normal([n_nodes_hl3]))}
 
-    output_layer = {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
-                      'biases': tf.Variable(tf.random_normal([n_classes]))}
+    output_layer =   {'weights':tf.Variable(tf.random_normal([n_nodes_hl3, n_classes])),
+                      'biases' :tf.Variable(tf.random_normal([n_classes]))}
 
-    # (input_data * weights) + biases
 
-    #summation
+    #do the linear combinations now
     l1 = tf.add(tf.matmul(data, hidden_1_layer['weights']), hidden_1_layer['biases'])
-    #activation function
     l1 = tf.nn.relu(l1)
 
     l2 = tf.add(tf.matmul(l1, hidden_2_layer['weights']), hidden_2_layer['biases'])
@@ -50,20 +44,23 @@ def neural_network_model(data):
     l3 = tf.nn.relu(l3)
 
     output = tf.add(tf.matmul(l3, output_layer['weights']), output_layer['biases'])
+
+    #print(Tensor.eval(output))
     return output
 
 
 def train_neural_network():
-    print(type(train_x[0]))
+    #print(train_x)
     prediction = neural_network_model(train_x)
-    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=prediction, labels=train_y))
+    cost = tf.reduce_sum(tf.square(tf.subtract(prediction, train_y)))
     optimizer = tf.train.AdamOptimizer().minimize(cost)
+    #cost = tf.reduce_mean(tf.nn.sparse_softmax_cross_entropy_with_logits(logits=prediction, labels=y))
 
-    # cycles feed forward + backprop
     hm_epochs = 10
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
+        #print(prediction.eval())
 
         for epoch in range(hm_epochs):
             epoch_loss = 0
@@ -76,16 +73,73 @@ def train_neural_network():
                 batch_x = np.array(train_x[start:end])
                 batch_y = np.array(train_y[start:end])
 
-                _, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+                #_, c = sess.run([optimizer, cost], feed_dict={x: batch_x, y: batch_y})
+                _, c = sess.run([optimizer, cost])
+                #prediction = neural_network_model(x)
+                #sess.run(z, feed_dict={z: y})
                 epoch_loss += c
                 i += batch_size
 
             print('Epoch', epoch+1, 'completed out of', hm_epochs, 'loss:', epoch_loss)
 
-        correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        print("train_y = ", train_y[:])
+        print("prediction = ", prediction.eval()[:])
 
-        accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        print('Accuracy:', accuracy.eval({x:test_x, y:test_y}))
+        value = (tf.divide(tf.subtract(prediction, train_y), prediction))
+        acc = value.eval()
+        accuracies = tf.reduce_mean(acc)
+        print(accuracies.eval()) #We want this to be as close to 0 as possible!
 
+        nums = []
+
+        for x in range(len(acc)):
+            nums.append(x)
+
+        '''
+        plotting
+        '''
+
+        '''
+        plt.plot(prediction.eval()[:], acc)
+        plt.xlabel('output')
+        plt.ylabel('percent difference')
+        plt.title('percent difference vs output')
+        plt.savefig('percentdiffvsoutput.png')
+        '''
+
+
+        fig, ax1 = plt.subplots()
+        ax1.plot(nums, acc[:], 'b-')
+        ax1.set_xlabel('Iterations')
+        ax1.set_ylabel('Percent Difference', color='b')
+        ax1.tick_params('y', colors='b')
+        ax2 = ax1.twinx()
+        ax2.plot(nums, train_y[:], 'r-')
+        ax2.set_ylabel('Expected Output', color='r')
+        ax2.tick_params('y', colors='r')
+
+        fig.tight_layout()
+
+        plt.title("Percent Difference Compared With Training Data")
+        plt.show()
+
+
+        # plt.plot(train_y[:], prediction.eval()[:], 'b', numsx, numsy, 'r')
+        # plt.xlabel("Expected Output")
+        # plt.ylabel("Network Output")
+        # plt.title("Network Output vs Expected Output")
+        # plt.legend(["Network output vs Expected output", "Y = X"],4)
+        #
+        # plt.show()
+
+
+
+
+
+
+        #correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+        #accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
+
+        #print('Accuracy:', accuracy.eval({x:test_x, y:test_y}))
 
 train_neural_network()
